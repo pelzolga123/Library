3c03a6fca0bb47e08afc602ab9555852a0b1b2fd
tundeiness <tundeiness@yahoo.co.uk>
2019-11-07 10:31:52 +0100

    run npm init, fixing app layout

--- script.js
+++ script.js
@@ -1,67 +1,64 @@
 class Book {
-    constructor(name, author, year, status){
-        this.name = name;
-        this.author = author;
-        this.year = year;
-        this.status = status;
-    }
-  
+  constructor (name, author, year, status) {
+    this.name = name
+    this.author = author
+    this.year = year
+    this.status = status
+  }
 }
 
 class Library {
-    static displayBooks(){
-        let myLibrary = [
-            {   name: 'A Study in Scarlet',
-                author:'Arthur Conan Doyle',
-                year: '1887',
-                status: true
-            }
-        ];
-        const library = myLibrary;
-        library.forEach((book) => Library.addBookToLibray(book));
-    } 
+  static displayBooks () {
+    const myLibrary = [
+      {
+        name: 'A Study in Scarlet',
+        author: 'Arthur Conan Doyle',
+        year: '1887',
+        status: true
+      }
+    ]
+    const library = myLibrary
+    library.forEach((book) => Library.addBookToLibray(book))
+  }
 
-    static addBookToLibray(book){
-        var readStat = document.createElement('input');
-        const list = document.querySelector('#library-books');
-        
-        const row = document.createElement('tr');
-        row.innerHTML = `
+  static addBookToLibray (book) {
+    var readStat = document.createElement('input')
+    const list = document.querySelector('#library-books')
+
+    const row = document.createElement('tr')
+    row.innerHTML = `
         <td>${book.name}</td>
         <td>${book.author}</td>
         <td>${book.year}</td>
-        `;
+        `
 
-        row.appendChild(readStat);
-        //readStat.setAttribute("type", "button");
-        readStat.classList.add('btn','btn-primary', 'read');
-        list.appendChild(row);   
-        
+    row.appendChild(readStat)
+    // readStat.setAttribute("type", "button");
+    readStat.classList.add('btn', 'btn-primary', 'read')
+    list.appendChild(row)
 
-        if(book.status) {
-            readStat.classList.add('btn','btn-success');    
-            readStat.value = 'Finished';
-          } else {
-            readStat.classList.add('btn','btn-primary');  
-            readStat.value = 'Unread';
-          }
-      
-          readStat.addEventListener('click', (e) => {
-            if(book.status) {
-              readStat.classList.remove('btn','btn-success');      
-              readStat.classList.add('btn','btn-primary');    
-              book.status = false;
-              readStat.value = 'Unread';
-            } else {
-              book.status = true; 
-              readStat.classList.remove('btn','btn-promary');  
-              readStat.classList.add('btn','btn-success');
-              readStat.value = 'Finished';
-            }
-        });
+    if (book.status) {
+      readStat.classList.add('btn', 'btn-success')
+      readStat.value = 'Finished'
+    } else {
+      readStat.classList.add('btn', 'btn-primary')
+      readStat.value = 'Unread'
     }
+
+    readStat.addEventListener('click', (e) => {
+      if (book.status) {
+        readStat.classList.remove('btn', 'btn-success')
+        readStat.classList.add('btn', 'btn-primary')
+        book.status = false
+        readStat.value = 'Unread'
+      } else {
+        book.status = true
+        readStat.classList.remove('btn', 'btn-promary')
+        readStat.classList.add('btn', 'btn-success')
+        readStat.value = 'Finished'
+      }
+    })
+  }
 }
 
-
-
-document.addEventListener('DOMContentLoaded', Library.displayBooks);
+document.addEventListener('DOMContentLoaded', Library.displayBooks)
