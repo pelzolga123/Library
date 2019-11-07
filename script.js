9224d295aa28fbadc2d84429fbbad4c969b0c19a
tundeiness <tundeiness@yahoo.co.uk>
2019-11-07 12:15:50 +0100

    setup library

--- script.js
+++ script.js
@@ -9,21 +9,23 @@ class Book {
 
 class Library {
   static displayBooks () {
-    const myLibrary = [
-      {
-        name: 'A Study in Scarlet',
-        author: 'Arthur Conan Doyle',
-        year: '1887',
-        status: true
-      }
-    ]
-    const library = myLibrary
-    library.forEach((book) => Library.addBookToLibray(book))
+    // const myLibrary = [
+    //   {
+    //     name: 'A Study in Scarlet',
+    //     author: 'Arthur Conan Doyle',
+    //     year: '1887',
+    //     status: true
+    //   }
+    // ]
+
+    const books = Storer.getBook()
+    // const library = myLibrary
+    books.forEach((book) => Library.addBooksToLibrary(book))
   }
 
-  static addBookToLibray (book) {
-    var readStat = document.createElement('input')
-    const list = document.querySelector('#library-books')
+  static addBooksToLibrary (book) {
+    const readStat = document.createElement('input')
+    const list = document.getElementById('library-books')
 
     const row = document.createElement('tr')
     row.innerHTML = `
@@ -31,7 +33,7 @@ class Library {
         <td>${book.author}</td>
         <td>${book.year}</td>
         `
-
+    list.appendChild(row)
     row.appendChild(readStat)
     // readStat.setAttribute("type", "button");
     readStat.classList.add('btn', 'btn-primary', 'read')
@@ -59,6 +61,72 @@ class Library {
       }
     })
   }
+
+  static clearFields () {
+    document.querySelector('#name').value = ''
+    document.querySelector('#author').value = ''
+    document.querySelector('#year').value = ''
+  }
 }
 
-document.addEventListener('DOMContentLoaded', Library.displayBooks)
+// Store Class: Handles Storage
+class Storer {
+  // getBook
+  static getBook () {
+    let books
+
+    if (localStorage.getItem('books') === null) {
+      books = []
+    } else {
+      books = JSON.parse(localStorage.getItem('books'))
+    }
+
+    return books
+  }
+
+  // addBook
+  static addBook (book) {
+    const buk = Storer.getBook()
+
+    buk.push(book)
+
+    localStorage.setItem('buk', JSON.stringify(buk))
+  }
+}
+
+document.addEventListener('DOMContentLoaded', Library.displayBooks())
+
+// event: add a book
+
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+  // prevent actual submission
+  e.preventDefault()
+
+  // get form values
+  const name = document.querySelector('#name').value
+  const author = document.querySelector('#author').value
+  const year = document.querySelector('#year').value
+
+  // validations
+
+  // if (name === '' || author === '' || year === '') {
+  //   Library.showAlerts('Please fill in all fields', 'danger')
+  // } else {
+  // instantiate a book
+  const newBook = new Book(name, author, year)
+
+  const lib = new Library()
+
+  const store = new Storer()
+  // Add Book to Library
+  lib.addBooksToLibrary(newBook)
+
+  // Add Book to store
+  store.addBook(newBook)
+
+  // success message
+  // UI.showAlerts('Book added', 'success')
+  // Clear form fields
+  lib.clearFields()
+  // }
+})
